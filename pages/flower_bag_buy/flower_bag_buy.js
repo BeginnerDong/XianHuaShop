16c88e828dce2aa320f91e094a0700189cff68f2
zhou136 <2980765157@qq.com>
2019-05-06 17:26:55 +0800

    XianHuaShop
    页面完成

--- pages/flower_bag_buy/flower_bag_buy.js
+++ pages/flower_bag_buy/flower_bag_buy.js
@@ -3,72 +3,20 @@ var api = new Api();
 const app = getApp();
 import {Token} from '../../utils/token.js';
 const token = new Token();
-Page({
 
-  /**
-   * 页面的初始数据
-   */
+Page({
   data: {
     
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onLoad: function () {
     
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  },
-  intoPath(e){
-    const self = this;
-    api.pathTo(api.getDataSet(e,'path'),'nav');
-  },
+	intoPathRedirect(e){
+	  const self = this;
+	  api.pathTo(api.getDataSet(e,'path'),'redi');
+	},
+	intoPath(e){
+	  const self = this;
+	  api.pathTo(api.getDataSet(e,'path'),'nav');
+	}
 })
